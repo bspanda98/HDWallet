95de063d86ff08e28e4173aba760ddb4e2600e67
LamND <lamminhhieua6@gmail.com>
2019-01-02 10:04:17 +0700

    set extension String and Data is internal (resolve conflict with web3swift)

--- HDWalletKit/Core/Extensions/String+Hex.swift
+++ HDWalletKit/Core/Extensions/String+Hex.swift
@@ -12,7 +12,7 @@ fileprivate var hexPrefix = "0x"
 
 extension String {
     
-    public func stripHexPrefix() -> String {
+    func stripHexPrefix() -> String {
         var hex = self
         if hex.hasPrefix(hexPrefix) {
             hex = String(hex.dropFirst(hexPrefix.count))
@@ -20,11 +20,11 @@ extension String {
         return hex
     }
     
-    public func addHexPrefix() -> String {
+    func addHexPrefix() -> String {
         return hexPrefix.appending(self)
     }
     
-    public func toHexString() -> String {
+    func toHexString() -> String {
         guard let data = data(using: .utf8) else {
             return ""
         }
@@ -33,11 +33,11 @@ extension String {
 }
 
 extension Data {
-    public func dataToHexString() -> String {
+    func dataToHexString() -> String {
         return map { String(format: "%02x", $0) }.joined()
     }
     
-    public static func fromHex(_ hex: String) -> Data? {
+    static func fromHex(_ hex: String) -> Data? {
         let string = hex.lowercased().stripHexPrefix()
         let array = Array<UInt8>(hex: string)
         if (array.count == 0) {
@@ -50,7 +50,7 @@ extension Data {
         return Data(array)
     }
     
-    public func constantTimeComparisonTo(_ other:Data?) -> Bool {
+    func constantTimeComparisonTo(_ other:Data?) -> Bool {
         guard let rhs = other else {return false}
         guard self.count == rhs.count else {return false}
         var difference = UInt8(0x00)
